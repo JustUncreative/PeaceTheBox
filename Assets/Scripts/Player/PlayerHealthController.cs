c00a3ad80c0ed1dd5fc56c1c2c525cd031eb029a
Igor Puchkov <ArminusHD@yandex.ru>
2021-06-27 18:15:25 +0300

    Add Player Health Controller

---
+++ Assets/Scripts/Player/PlayerHealthController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    private float currentHealth;
+    public static PlayerHealthController instance;
+    [SerializeField] private float maxHealth;
+    private void Awake() {
+        instance = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ReduceHealthPoints(float Points){
+        if(isAlive()){
+            currentHealth -= Points;
+        }
+    }
+    public void AddHealthPoints(float Points){
+        if(isAlive() && currentHealth < maxHealth){
+            currentHealth += Points;
+        }
+        editCurrentHealth();
+    }
+    public bool isAlive(){
+        if(currentHealth > 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public void editCurrentHealth(){
+        if(currentHealth > maxHealth){
+            currentHealth = maxHealth;
+        }
+    }
+}

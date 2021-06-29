c0144c0fe97b40a7c0ce0bbe2a1df8b472609944
Igor Puchkov <ArminusHD@yandex.ru>
2021-06-29 16:33:00 +0300

    Offset groundTIleMap Position

--- Assets/Scripts/Player/groundTilemap.cs
+++ Assets/Scripts/Player/groundTilemap.cs
@@ -5,28 +5,42 @@ using UnityEngine;
 public class groundTilemap : MonoBehaviour
 {
     public static groundTilemap instance;
-    private Vector3 borderPos;
     private Vector3 position;
+    private float offsetPos;
+    private bool Right;
+    [SerializeField] private float smearSpeed;
     private void Awake() {
         instance = this;
-        
-        float horizontal = Movement.instance.horizontal;
-        float offsetPos = horizontal * Movement.instance.transform.localScale.x/2 * 0.95f;
-        Vector3 position = Movement.instance.transform.position;
-        Vector3 borderPos = new Vector3(position.x + offsetPos, transform.position.y, position.z);
+
     }
     public Transform groundTilemapPosition;
     private void FixedUpdate() {
-        //float horizontal = Movement.instance.horizontal;
-        float offsetPos = Movement.instance.transform.localScale.x/2 * 0.95f;
+
+        /*float horizontal = Movement.instance.horizontal;
+
+        if(horizontal != 0){
+            offsetPos = horizontal/System.Math.Abs(horizontal) * Movement.instance.transform.localScale.x/2 * 0.9f;
+        }else {
+            offsetPos = horizontal * Movement.instance.transform.localScale.x/2 * 0.9f;
+        }
+        
         Vector3 position = Movement.instance.transform.position;
 
-        transform.position = new Vector3(position.x - offsetPos, transform.position.y, position.z);
+        transform.position = new Vector3(position.x + offsetPos, transform.position.y, position.z);*/
 
-        while(transform.position != borderPos){
-            position = Movement.instance.transform.position;
-
-            transform.position = new Vector3(transform.position.x + 0.01f, transform.position.y, transform.position.z);
+        if(transform.localPosition.x > 0.45f){
+            Right = false;
+        }else if(transform.localPosition.x < -0.45f){
+            Right = true;
         }
+
+        if(!Right){
+            transform.localPosition = new Vector3(transform.localPosition.x - smearSpeed, transform.localPosition.y, transform.localPosition.z);
+        }else if(Right){
+            transform.localPosition = new Vector3(transform.localPosition.x + smearSpeed, transform.localPosition.y, transform.localPosition.z);
+        }
+
     }
+
+        
 }
